d64343c6b2bade2def3ab05ec94e790b67f95e82
Yuurin <byyuurin@gmail.com>
2022-03-28 16:59:36 +0800

    chore: rename [skip ci]

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, presetAttributify, presetIcons, presetWind } from 'unocss'
 import transformerDirective from '@unocss/transformer-directives'
-import presetUser from './src/utils/unocss-preset'
+import presetUser from './src/utils/unocss-preset-user'
 
 export default defineConfig({
   shortcuts: [{ 'theme-container': 'text-gray-700 bg-white dark:bg-dark-800 dark:text-light-700' }],
